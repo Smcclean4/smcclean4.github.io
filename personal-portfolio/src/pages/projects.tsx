8564eef2aea37557e5b224973fe8d5762ae68901
Smcclean4 <lsaudim5@outlook.com>
2023-04-12 16:16:44 -0700

    projects
    added video for foodlux project.

--- personal-portfolio/src/pages/projects.tsx
+++ personal-portfolio/src/pages/projects.tsx
@@ -11,7 +11,13 @@ const projects = () => {
   return (
     <Layout>
       <div className={loading ? "w-3/4 lg:w-2/4 h-3/4 flex flex-col animate-fadingRev" : "w-3/4 lg:w-2/4 h-3/4 flex flex-col"}>
-        <p className={loading ? "text-2xl sm:text-3xl font-mono animate-extension1" : "text-2xl sm:text-3xl font-mono"}><span className="text-8xl sm:text-9xl font-serif text-gray-800 opacity-70"><i>1.</i></span> This is <a href="/projects/foodlux"><b className="underline"><i>Foodlux.</i></b></a></p>
+        <div className="flex flex-row items-end">
+          <span className="text-8xl sm:text-9xl font-serif text-gray-800 opacity-70"><i>1.</i></span>
+          <div>
+            <p className={loading ? "text-2xl sm:text-3xl font-mono animate-extension1 mb-2" : "text-2xl sm:text-3xl font-mono mb-2"}>This is <a href="/projects/foodlux"><b className="underline"><i>Foodlux.</i></b></a></p>
+            <p className="text-md sm:text-lg text-gray-500"><i>Full-Stack (MERN) food service web application.</i></p>
+          </div>
+        </div>
       </div>
     </Layout>
   )
--- personal-portfolio/src/pages/projects/foodlux.tsx
+++ personal-portfolio/src/pages/projects/foodlux.tsx
@@ -1,10 +1,17 @@
 import { Layout } from "@/components/Layout"
+import { useState, useEffect } from "react";
 
 const foodlux = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+  }, [])
+
   return (
     <Layout>
-      <div className="flex flex-col sm:flex-row w-full h-full sm:bg-gradient-to-br from-black from-50% to-white to-50%">
-        <div className="w-full sm:w-1/2 h-full flex flex-col items-center pt-10">
+      <div className="flex flex-col lg:flex-row w-full h-full lg:bg-gradient-to-br from-black from-50% to-white to-50%">
+        <div className="w-full lg:w-1/2 h-full flex flex-col items-center pt-10">
           <p className="text-5xl mb-10 font-mono underline">Foodlux</p>
           <p className="w-3/4 text-2xl font-extralight">An example food servicing website where you can order food, browse menus,and  order food using your preferred payment method. You'll receive order information after putting in your checkout information, and you can track the status of your order from the comfort of your own home.</p>
           <ul className="my-10 flex flex-row flex-wrap w-3/4 justify-center">
@@ -16,8 +23,9 @@ const foodlux = () => {
             <a target="_blank" href="https://www.nodejs.org"><img className="m-3 h-15 animate-pulse" src="https://img.icons8.com/color/48/000000/nodejs.png" /></a>
           </ul>
         </div>
-        <div className="text-black w-full sm:w-1/2 h-full flex justify-center items-center bg-white sm:bg-inherit">
-          <p>This will contain lthe white split.</p>
+        <div className={loading ? "animate-fadingRev text-black w-full lg:w-1/2 h-full flex flex-col items-center justify-end bg-white lg:bg-inherit" : "text-black w-full lg:w-1/2 h-full flex flex-col items-center justify-end bg-white lg:bg-inherit"}>
+          <video controls src={"/IMG_0276.MOV"} className="w-full h-auto p-6" />
+          <p className="font-mono text-md md:text-xl my-4">You can find this project <a href="" className="underline">here.</a></p>
         </div>
       </div>
     </Layout>
